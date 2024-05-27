2fb9a60435330d2ae1fb712035045eea10cb5eb2
olaleykhan <realjoy9@gmail.com>
2024-05-27 17:36:09 +0100

    init nebulon

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import services from "./services/index.ts"
+
+function App() {
+
+  console.log(services.valueOf() , null)
+
+  return (
+    <>
+      <h1> Whot!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/services/index.ts
@@ -0,0 +1,3 @@
+console.log('services will be in here. game, auth, http, etc.')
+
+export default{}
