33d83d9851b591c136adccf5fec9d7a4ec46603e
lekan_dev <realjoy9@gmail.com>
2024-07-14 17:42:09 +0100

    bring our web server into our monorepo

---
+++ server/ttt/src/main.ts
@@ -0,0 +1,25 @@
+// import koa from 'koa';
+import { Server, Origins } from 'boardgame.io/server';
+import {tttCore} from '@afg/ttt-core';
+
+const PORT = Number(process.env.PORT) || 8082;
+
+const server = Server({
+  games: [tttCore],
+  origins: [Origins.LOCALHOST],
+});
+
+
+
+server.router.get('/', (ctx, next)=>{
+  ctx
+  ctx.body = "<h1> Nebulon Web:Socket Server. Welcome!!! </h1>"
+})
+server.router.get('/ok', (ctx, next)=>{
+  ctx.body = "<h1>Server OK!!! </h1>"
+})
+
+
+server.app.createContext
+
+server.run(PORT, ()=>console.log(tttCore.name,"server is running on port : ", PORT));
