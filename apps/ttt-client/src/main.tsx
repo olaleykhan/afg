21b1aea260904bd18d0b84fea286ef5368519db5
lekan_dev <realjoy9@gmail.com>
2024-07-15 18:17:45 +0100

    prettier

--- apps/ttt-client/src/main.tsx
+++ apps/ttt-client/src/main.tsx
@@ -1,12 +1,10 @@
-import "./styling/robotoFonts"
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import './tw.css'
+import './styling/robotoFonts';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import './tw.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-    <App />
-);
+root.render(<App />);
